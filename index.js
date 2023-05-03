f1278c0a3705018637566b84c7aa0b402523fecb
Ty Blackwell <tyeverettblackwell@gmail.com>
2023-05-03 15:44:30 -0400

    pass test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.querySelector("p#text");
+  element.textContent = "This is really cool!";
+});
